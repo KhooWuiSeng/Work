4c3746a07da52036915d984262205081f1503bfc
KhooWuiSeng <khoowuiseng@gmail.com>
2023-12-06 21:46:33 +0800

    Create parser_2.py

---
+++ parser_2.py
@@ -0,0 +1,13 @@
+#   py -m pip install parse
+import parse
+
+with open('Parsing exercise/headers.txt') as f:
+    headers = f.readlines()
+
+results = []
+for header in headers:
+    results.append(parse.parse('##INFO=<ID={ID},Number={Number},Type={Type},Description="{Description}">\n',header).named)
+
+print (results[:2])
+print (type(results))
+print (type(results[0]))
