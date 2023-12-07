5e8903e0c387ba4e65169fd8c17e71a3b8f35447
KhooWuiSeng <khoowuiseng@gmail.com>
2023-12-07 09:21:36 +0800

    Updated with working version

--- parser_2.py
+++ parser_2.py
@@ -1,5 +1,6 @@
 #   py -m pip install parse
 import parse
+import pandas as pd
 
 with open('Parsing exercise/headers.txt') as f:
     headers = f.readlines()
@@ -8,6 +9,10 @@ results = []
 for header in headers:
     results.append(parse.parse('##INFO=<ID={ID},Number={Number},Type={Type},Description="{Description}">\n',header).named)
 
-print (results[:2])
-print (type(results))
-print (type(results[0]))
+#   print (results[:2])
+#   print (type(results))
+#   print (type(results[0]))
+
+df = pd.DataFrame.from_dict(results)
+
+print(df)
